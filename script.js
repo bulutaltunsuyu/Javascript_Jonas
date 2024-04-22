94e7bbe28f172e5f6d4fb9acd82b816c80172f57
bulutaltunsuyu <marmarisrealestate@hotmail.com>
2024-04-22 20:38:02 +0300

    j2

---
+++ script.js
@@ -0,0 +1,21 @@
+/* const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(massMark, massJohn); */
+
+const firstName = "Jonas";
+const job = "teacher";
+const birtYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birtYear) + " year old " + job + "!";
+
+console.log(jonas);
+
+const jonasNew = `I'am ${firstName} a ${year - birtYear} year old ${job}!`;
+console.log(jonasNew);
